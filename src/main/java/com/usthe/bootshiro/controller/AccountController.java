19d665c5ef663d7fc6f3090bec25463142c74bb0
tomsun28 <tomsun28@outlook.com>
2018-04-22 11:07:22 +0800

    解决warning

--- src/main/java/com/usthe/bootshiro/controller/AccountController.java
+++ src/main/java/com/usthe/bootshiro/controller/AccountController.java
@@ -12,7 +12,6 @@ import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
