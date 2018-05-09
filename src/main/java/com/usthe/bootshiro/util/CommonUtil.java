e8b9ca820e3360c439a89c9c5f4a35a3737ada05
William <825390941@qq.com>
2018-05-09 14:44:07 +0800

    buffer->builder

--- src/main/java/com/usthe/bootshiro/util/CommonUtil.java
+++ src/main/java/com/usthe/bootshiro/util/CommonUtil.java
@@ -17,7 +17,7 @@ public class CommonUtil {
     public static String getRandomString(int length) {
         String base = "abcdefghijklmnopqrstuvwxyz0123456789";
         Random random = new Random();
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < length; i++) {
             int number = random.nextInt(base.length());
             sb.append(base.charAt(number));
